9246589d4d36b084acf4374d5f81063b3c46af83
ru42fur <ru42fur>
2025-11-21 09:50:22 +0100

    Update package documentation

--- src/main/java/de/bcxp/challenge/package-info.java
+++ src/main/java/de/bcxp/challenge/package-info.java
@@ -1,6 +1,17 @@
 /**
  * The root package for the data challenge containing the entry point
  * {@link de.bcxp.challenge.App} and containing the resource files.
- * Please create and document your own package structure appropriate to your design.
+ * 
+ * Sub-packages are structured by responsibility and domain:
+ *
+ * - {@code de.bcxp.challenge.io}
+ *   Contains abstractions and implementations for reading tabular data
+ *   (e.g. {@link de.bcxp.challenge.io.TableReader} and CSV readers).
+ *
+ * - {@code de.bcxp.challenge.weather}
+ *   Contains domain and analysis logic for the weather dataset.
+ *
+ * - {@code de.bcxp.challenge.countries}
+ *   Contains domain and analysis logic for the countries dataset.
  */
 package de.bcxp.challenge;
